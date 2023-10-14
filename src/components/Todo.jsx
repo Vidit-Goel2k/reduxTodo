5fa48e5213a54ef9305373ae6d9643e10ad36eef
Vidit <viditgoel2k@gmail.com>
2023-10-14 18:26:53 +0530

    redux toolkit todo

---
+++ src/components/Todo.jsx
@@ -0,0 +1,28 @@
+import { useSelector, useDispatch } from "react-redux"
+import { removeTodo } from "../features/todo/todoSlice"
+
+const Todo = () => {
+  const todos = useSelector(state => state.todos)
+  const dispatch = useDispatch()
+
+  return (
+    <>
+      <div>
+        {
+          todos.map((todo) => (
+            <li key={todo.id}>
+              {todo.text}
+              <button
+                onClick={()=>{dispatch(removeTodo(todo.id))}}
+              >
+                Remove Todo
+              </button>
+            </li>
+          ))
+        }
+      </div>
+    </>
+  )
+}
+
+export default Todo
